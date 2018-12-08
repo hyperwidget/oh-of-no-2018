447ca1a029e7625806c28894dbc459e547dc7434
Hunter Jansen <rawkamatic@gmail.com>
2018-12-08 01:43:43 -0500

    8b

--- days/day8/solution.js
+++ days/day8/solution.js
@@ -11,7 +11,6 @@ const processNode = (inputs, cursor, total) => {
   }
 
   for (let i = 0; i < metaCount; i++) {
-    console.log(inputs[newCursor], 'adding to sum')
     newTotal += parseInt(inputs[newCursor])
     newCursor++
   }
@@ -19,40 +18,56 @@ const processNode = (inputs, cursor, total) => {
   return { total: newTotal, cursor: newCursor }
 }
 
-const processNode2 = (inputs, cursor, total) => {
+const processNode2 = (inputs, cursor, total, index) => {
   const childrenCount = inputs[cursor]
   const metaCount = inputs[cursor + 1]
+  const children = []
   let newCursor = cursor + 2
-  let newTotal = total
+  let nodeTotal = 0
 
   for (let i = 0; i < childrenCount; i++) {
-    const response = processNode(inputs, newCursor, total)
-    newTotal += response.total
+    const response = processNode2(inputs, newCursor, total, index + 1)
     newCursor = response.cursor
+    children.push(response)
   }
 
-  for (let i = 0; i < metaCount; i++) {
-    console.log(inputs[newCursor], 'adding to sum')
-    newTotal += parseInt(inputs[newCursor])
-    newCursor++
+  if (parseInt(childrenCount) === 0) {
+    for (let i = 0; i < metaCount; i++) {
+      nodeTotal += parseInt(inputs[newCursor])
+      newCursor++
+    }
+
+    return { total: nodeTotal, cursor: newCursor, children: [] }
+  } else {
+    for (let i = 0; i < metaCount; i++) {
+      const metaIdx = parseInt(inputs[newCursor]) - 1
+      if (metaIdx + 1 <= children.length) {
+        nodeTotal += children[metaIdx].total
+      }
+      newCursor++
+    }
   }
 
-  return { total: newTotal, cursor: newCursor }
+  return { total: nodeTotal, cursor: newCursor, children }
 }
 
 export default {
   a: input => {
     const inputs = input[0].split(' ')
 
-    const metaDataTotal = processNode(inputs, 0, 0)
+    const metaDataTotal = processNode(inputs, 0, 0, 1)
 
     return metaDataTotal.total
   },
   b: input => {
     const inputs = input[0].split(' ')
 
-    const metaDataTotal = processNode(inputs, 0, 0)
+    const metaDataTotal = processNode2(inputs, 0, 0)
 
     return metaDataTotal.total
+    // 318 too low
+    // 0 - wrong 🙃
+    // Fumbled around, discovered that I was misunderstanding
+    // and was nuking the total of a node if any of it's children were empty
   }
 }
