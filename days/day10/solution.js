0212c762b1daf2af8fa457ee558c048dda17892e
Hunter Jansen <rawkamatic@gmail.com>
2018-12-10 17:05:49 -0500

    clean up

--- days/day10/solution.js
+++ days/day10/solution.js
@@ -1,131 +1,92 @@
 import { cloneDeep } from "lodash";
 
-export default {
-  a: inputs => {
-    const points = [];
-    inputs.forEach(input => {
-      const splitted = input
-        .replace("position=<", "")
-        .replace("velocity=", "")
-        .split("> ");
-      const position = splitted[0].split(", ");
-      const velocity = splitted[1]
-        .replace("<", "")
-        .replace(">", "")
-        .split(", ");
+export const process = inputs => {
+  const points = [];
+  inputs.forEach(input => {
+    const splitted = input
+      .replace("position=<", "")
+      .replace("velocity=", "")
+      .split("> ");
+    const position = splitted[0].split(", ");
+    const velocity = splitted[1]
+      .replace("<", "")
+      .replace(">", "")
+      .split(", ");
 
-      const point = {
-        startingPosition: [parseInt(position[0]), parseInt(position[1])],
-        position: [parseInt(position[0]), parseInt(position[1])],
-        velocity: [parseInt(velocity[0]), parseInt(velocity[1])]
-      };
-      points.push(point);
-    });
+    const point = {
+      startingPosition: [parseInt(position[0]), parseInt(position[1])],
+      position: [parseInt(position[0]), parseInt(position[1])],
+      velocity: [parseInt(velocity[0]), parseInt(velocity[1])]
+    };
+    points.push(point);
+  });
 
-    let isGrowing = false;
-    let lastLength = 999999;
-    let lastPoints = [];
-    let count = 0;
+  let isGrowing = false;
+  let lastLength = 999999;
+  let lastPoints = [];
+  let count = 0;
+  let grid = [];
 
-    while (!isGrowing) {
-      const lowestX = points.reduce(
-        (lowest, point) =>
-          point.position[0] < lowest ? point.position[0] : lowest,
-        0
-      );
-      const highestX = points.reduce(
-        (highest, point) =>
-          point.position[0] > highest ? point.position[0] : highest,
-        0
-      );
-      const lowestY = points.reduce(
-        (lowest, point) =>
-          point.position[1] < lowest ? point.position[1] : lowest,
-        0
-      );
-      const highestY = points.reduce(
-        (highest, point) =>
-          point.position[1] > highest ? point.position[1] : highest,
-        0
-      );
+  while (!isGrowing) {
+    const lowestX = points.reduce(
+      (lowest, point) =>
+        point.position[0] < lowest ? point.position[0] : lowest,
+      99999
+    );
+    const highestX = points.reduce(
+      (highest, point) =>
+        point.position[0] > highest ? point.position[0] : highest,
+      0
+    );
+    const lowestY = points.reduce(
+      (lowest, point) =>
+        point.position[1] < lowest ? point.position[1] : lowest,
+      99999
+    );
+    const highestY = points.reduce(
+      (highest, point) =>
+        point.position[1] > highest ? point.position[1] : highest,
+      0
+    );
 
-      const gridXSize = highestX;
-      const gridYSize = highestY;
+    const gridXSize = highestX - lowestX;
+    const gridYSize = highestY - lowestY;
 
-      if (gridYSize === 10) {
-        isGrowing = true;
-        const grid = Array(gridXSize + 1).fill([]);
-        for (let i = 0; i < grid.length; i++) {
-          grid[i] = Array(gridYSize).fill(".");
-        }
+    if (gridYSize > lastLength) {
+      isGrowing = true;
+      grid = Array(gridXSize + 1).fill([]);
 
-        lastPoints.forEach(point => {
-          let x = 0;
-          let y = 0;
-
-          x = point.position[0] + (0 - lowestX);
-          y = point.position[1] + (0 - lowestY);
-
-          // console.log(point.position[0], point.position[1])
-          // console.log(x, y)
-          grid[y][x] = "x";
-        });
-        console.table(grid);
+      grid = Array(gridYSize).fill([]);
+      for (let i = 0; i < grid.length; i++) {
+        grid[i] = Array(gridXSize).fill(" ");
       }
 
-      if (gridYSize > lastLength) {
-        isGrowing = true;
-        let grid = Array(gridXSize + 1).fill([]);
+      lastPoints.forEach(point => {
+        let x = 0;
+        let y = 0;
 
-        // console.log(gridYSize);
-        // console.log(gridXSize);
-        // grid = Array(gridYSize).fill([]);
-        // for (let i = 0; i < grid.length; i++) {
-        //   grid[i] = Array(gridXSize).fill(" ");
-        // }
+        x = point.position[0] - lowestX;
+        y = point.position[1] - lowestY;
 
-        // lastPoints.forEach(point => {
-        //   let x = 0;
-        //   let y = 0;
-
-        //   x = point.position[0];
-        //   y = point.position[1];
-
-        //   if (x === 2 && y === 1) {
-        //     console.log(point);
-        //   }
-
-        //   // console.log(point.position[0], point.position[1])
-        //   // console.log(x, y)
-        //   grid[y][x] = "x";
-        // });
-        // console.table(grid);
-      } else {
-        lastLength = gridYSize;
-        lastPoints = cloneDeep(points);
-      }
-
-      // if (!lastLength || grid[0].length < lastLength) {
-      //   console.log(lastLength)
-      //   lastLength = grid[0].length
-      // } else {
-      //   console.log('exit')
-      //   console.log(grid[0].length)
-      //   isGrowing = true
-      // }
-      // console.table(grid)
-
-      points.forEach(point => {
-        point.position[0] += point.velocity[0];
-        point.position[1] += point.velocity[1];
+        grid[y][x] = "x";
       });
-      count++;
+    } else {
+      lastLength = gridYSize;
+      lastPoints = cloneDeep(points);
     }
 
-    console.log(count - 2);
-    return 0;
-  },
-  b: input => {
-    return input;
+    points.forEach(point => {
+      point.position[0] += point.velocity[0];
+      point.position[1] += point.velocity[1];
+    });
+    count++;
   }
+
+  console.table(grid);
+  return count - 2;
+};
+
+export default {
+  a: process,
+  b: process
 };
--- dist/runner.js
+++ dist/runner.js
@@ -1527,105 +1527,73 @@ exports.default = _default;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports.default = exports.process = void 0;
 
 var _lodash = require("lodash");
 
-var _default = {
-  a: inputs => {
-    const points = [];
-    inputs.forEach(input => {
-      const splitted = input.replace("position=<", "").replace("velocity=", "").split("> ");
-      const position = splitted[0].split(", ");
-      const velocity = splitted[1].replace("<", "").replace(">", "").split(", ");
-      const point = {
-        startingPosition: [parseInt(position[0]), parseInt(position[1])],
-        position: [parseInt(position[0]), parseInt(position[1])],
-        velocity: [parseInt(velocity[0]), parseInt(velocity[1])]
-      };
-      points.push(point);
-    });
-    let isGrowing = false;
-    let lastLength = 999999;
-    let lastPoints = [];
-    let count = 0;
+const process = inputs => {
+  const points = [];
+  inputs.forEach(input => {
+    const splitted = input.replace("position=<", "").replace("velocity=", "").split("> ");
+    const position = splitted[0].split(", ");
+    const velocity = splitted[1].replace("<", "").replace(">", "").split(", ");
+    const point = {
+      startingPosition: [parseInt(position[0]), parseInt(position[1])],
+      position: [parseInt(position[0]), parseInt(position[1])],
+      velocity: [parseInt(velocity[0]), parseInt(velocity[1])]
+    };
+    points.push(point);
+  });
+  let isGrowing = false;
+  let lastLength = 999999;
+  let lastPoints = [];
+  let count = 0;
+  let grid = [];
 
-    while (!isGrowing) {
-      const lowestX = points.reduce((lowest, point) => point.position[0] < lowest ? point.position[0] : lowest, 0);
-      const highestX = points.reduce((highest, point) => point.position[0] > highest ? point.position[0] : highest, 0);
-      const lowestY = points.reduce((lowest, point) => point.position[1] < lowest ? point.position[1] : lowest, 0);
-      const highestY = points.reduce((highest, point) => point.position[1] > highest ? point.position[1] : highest, 0);
-      const gridXSize = highestX;
-      const gridYSize = highestY;
+  while (!isGrowing) {
+    const lowestX = points.reduce((lowest, point) => point.position[0] < lowest ? point.position[0] : lowest, 99999);
+    const highestX = points.reduce((highest, point) => point.position[0] > highest ? point.position[0] : highest, 0);
+    const lowestY = points.reduce((lowest, point) => point.position[1] < lowest ? point.position[1] : lowest, 99999);
+    const highestY = points.reduce((highest, point) => point.position[1] > highest ? point.position[1] : highest, 0);
+    const gridXSize = highestX - lowestX;
+    const gridYSize = highestY - lowestY;
 
-      if (gridYSize === 10) {
-        isGrowing = true;
-        const grid = Array(gridXSize + 1).fill([]);
+    if (gridYSize > lastLength) {
+      isGrowing = true;
+      grid = Array(gridXSize + 1).fill([]);
+      grid = Array(gridYSize).fill([]);
 
-        for (let i = 0; i < grid.length; i++) {
-          grid[i] = Array(gridYSize).fill(".");
-        }
-
-        lastPoints.forEach(point => {
-          let x = 0;
-          let y = 0;
-          x = point.position[0] + (0 - lowestX);
-          y = point.position[1] + (0 - lowestY); // console.log(point.position[0], point.position[1])
-          // console.log(x, y)
-
-          grid[y][x] = "x";
-        });
-        console.table(grid);
+      for (let i = 0; i < grid.length; i++) {
+        grid[i] = Array(gridXSize).fill(" ");
       }
 
-      if (gridYSize > lastLength) {
-        isGrowing = true;
-        let grid = Array(gridXSize + 1).fill([]); // console.log(gridYSize);
-        // console.log(gridXSize);
-        // grid = Array(gridYSize).fill([]);
-        // for (let i = 0; i < grid.length; i++) {
-        //   grid[i] = Array(gridXSize).fill(" ");
-        // }
-        // lastPoints.forEach(point => {
-        //   let x = 0;
-        //   let y = 0;
-        //   x = point.position[0];
-        //   y = point.position[1];
-        //   if (x === 2 && y === 1) {
-        //     console.log(point);
-        //   }
-        //   // console.log(point.position[0], point.position[1])
-        //   // console.log(x, y)
-        //   grid[y][x] = "x";
-        // });
-        // console.table(grid);
-      } else {
-        lastLength = gridYSize;
-        lastPoints = (0, _lodash.cloneDeep)(points);
-      } // if (!lastLength || grid[0].length < lastLength) {
-      //   console.log(lastLength)
-      //   lastLength = grid[0].length
-      // } else {
-      //   console.log('exit')
-      //   console.log(grid[0].length)
-      //   isGrowing = true
-      // }
-      // console.table(grid)
-
-
-      points.forEach(point => {
-        point.position[0] += point.velocity[0];
-        point.position[1] += point.velocity[1];
+      lastPoints.forEach(point => {
+        let x = 0;
+        let y = 0;
+        x = point.position[0] - lowestX;
+        y = point.position[1] - lowestY;
+        grid[y][x] = "x";
       });
-      count++;
+    } else {
+      lastLength = gridYSize;
+      lastPoints = (0, _lodash.cloneDeep)(points);
     }
 
-    console.log(count - 2);
-    return 0;
-  },
-  b: input => {
-    return input;
+    points.forEach(point => {
+      point.position[0] += point.velocity[0];
+      point.position[1] += point.velocity[1];
+    });
+    count++;
   }
+
+  console.table(grid);
+  return count - 2;
+};
+
+exports.process = process;
+var _default = {
+  a: process,
+  b: process
 };
 exports.default = _default;
 },{}],"days/day10/test.js":[function(require,module,exports) {
